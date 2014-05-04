a351670259765adb38bef2ac4d4389262dfd1ef9
xhyan <imissyou.xhyan@gmail.com>
2014-05-05 00:05:00 +0800

    实现了mybatis的添加功能 实现了数据表外键自增

--- src/main/java/com/xhyan/base/controller/UserController.java
+++ src/main/java/com/xhyan/base/controller/UserController.java
@@ -1,24 +1,38 @@
 package com.xhyan.base.controller;
 
 import com.xhyan.base.model.User;
+import com.xhyan.base.service.UserService;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 
 /**
  * Created by xhyan on 14-5-4.
  */
 @Controller
+@RequestMapping("/")
+@ContextConfiguration(value = "classpath:/conf/spring-config.xml")
 public class UserController
 {
+    @Autowired
+    User user;
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     private String addUser(@Param("username")String username, @Param("password")String password, @Param("mail")String mail, ModelMap model)
     {
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setMail(mail);
+        userService.insertUser(user);
+        System.out.println("添加用户成功！！");
         model.addAttribute("username", username);
         model.addAttribute("password", password);
         model.addAttribute("mail", mail);
